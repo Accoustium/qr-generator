0da08dbb8d4134c71307f911799720ef76d4b0af
tim.pogue <tim.pogue@cybera.net>
2021-01-17 14:40:02 -0600

    Make changes from generator class to import encoding classes.

--- qrgenerator/generator.py
+++ qrgenerator/generator.py
@@ -1,4 +1,7 @@
-import encoding
+from .encoding.alphanumeric import Alphanumeric
+from .encoding.kanji import Kanji
+from .encoding.byte import Byte
+from .encoding.numeric import Numeric
 import correction
 
 
