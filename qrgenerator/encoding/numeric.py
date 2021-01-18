e668f686b95dabc0e133189ab0da4ff0e81f9e89
tim.pogue <tim.pogue@cybera.net>
2021-01-17 23:40:51 -0600

    Create function in numeric to handle short numbers in tripple groups.

--- qrgenerator/encoding/numeric.py
+++ qrgenerator/encoding/numeric.py
@@ -1,6 +1,7 @@
 from .encoding import Encoding
 from .formatting import numeric
 
+
 class Numeric(Encoding):
     def __init__(self, number: int, correction: str, version: int = None):
         super().__init__(
@@ -17,13 +18,20 @@ class Numeric(Encoding):
         return f"Numeric(number={self.decoded_string}, error_correction={self.error_correction})"
 
     def encode(self):
-        # TODO - fix encoding to handle one and two zeros in grouping of three numbers.
         encoding = [
             self.decoded_string[_:_+3]
             for _ in range(0, self.character_length + 1, 3)
         ]
 
-        encoded_word = ''.join(list(map(lambda x: bin(int(x))[2:], encoding)))
+        encoded_word = ''.join(list(map(self.encoding_number, encoding)))
         encoded_word = encoded_word + '0' * (7 - (len(str(encoded_word)) % 8))
 
         return encoded_word
+
+    def encoding_number(self, number):
+        if number.startswith('00') or len(number) == 1:
+            return bin(int(number))[2:].zfill(4)
+        elif number.startswith('0') or len(number) == 2:
+            return bin(int(number))[2:].zfill(7)
+        else:
+            return bin(int(number))[2:].zfill(10)
