6e7f7f2ae687f8ceb545a9e5cb0f880e4648f0ef
tim.pogue <tim.pogue@cybera.net>
2021-01-18 15:57:54 -0600

    create testing to confirm alphanumeric encoding.

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -1,4 +1,23 @@
 import pytest
+from qrgenerator.encoding.alphanumeric import Alphanumeric
 
 
-test_
+@pytest.fixture(scope='module')
+def alpha_fixture():
+    return Alphanumeric('HELLO WORLD', 'H')
+
+
+def test_alpha_mode(alpha_fixture):
+    assert alpha_fixture.mode_indicator == '0010'
+
+
+def test_alpha_character_count(alpha_fixture):
+    assert alpha_fixture.character_count == '000001011'
+
+
+def test_alpha_word_encoding(alpha_fixture):
+    assert alpha_fixture.encode() == '0110000101101111000110100010111001011011100010011010100001101'
+
+
+def test_alpha_string_encoding(alpha_fixture):
+    assert str(alpha_fixture) == '00100000010110110000101101111000110100010111001011011100010011010100001101000000'
