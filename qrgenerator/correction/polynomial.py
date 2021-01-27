a3b4b4dbc6c90d91183fce9312eebaeb49679f66
tim.pogue <tim.pogue@cybera.net>
2021-01-27 15:05:46 -0600

    Work Term to update way it expresses a term in a string.  And add multiplication for Terms and Polynomials.

--- qrgenerator/correction/polynomial.py
+++ qrgenerator/correction/polynomial.py
@@ -1,5 +1,8 @@
 import re
-# from .galois import Galois
+from .galois import Galois
+
+
+GF = Galois()
 
 
 class Term:
@@ -16,12 +19,42 @@ class Term:
 
     def __str__(self):
         superscript = str.maketrans("0123456789", "⁰¹²³⁴⁵⁶⁷⁸⁹")
-        coe = self.coefficient if self.coefficient != 1 else ''
+        if self.coefficient == 1:
+            if self.exponent == 0:
+                coe = self.coefficient
+            else:
+                coe = ''
+        else:
+            coe = self.coefficient
 
-        var = self.variable if self.exponent != 0 else ''
-        exp = self.exponent if self.exponent != 0 else ''
+        if self.exponent == 0:
+            var = ''
+            exp = ''
+        elif self.exponent == 1:
+            var = self.variable
+            exp = ''
+        else:
+            var = self.variable
+            exp = self.exponent
 
         return f"{coe}{var}{str(exp).translate(superscript)}"
+
+    def __mul__(self, other):
+        if isinstance(other, Term):
+            x1 = GF.find_exponent(self.coefficient)
+            y1 = GF.find_exponent(other.coefficient)
+            z1 = x1 + y1
+            if z1 >= 256:
+                z1 = z1 % 255
+            return Term(GF.find_integer(z1), self.exponent + other.exponent)
+
+    def __gt__(self, other):
+        if isinstance(other, Term):
+            return self.exponent > other.exponent
+
+    def __lt__(self, other):
+        if isinstance(other, Term):
+            return self.exponent < other.exponent
 
     @classmethod
     def create_from_str(cls, single_term: str):
@@ -40,10 +73,44 @@ class Polynomial:
         self.equation = list()
 
     def __repr__(self):
-        return f"Polynomial()"
+        return f"Polynomial({' + '.join(map(str, self.equation))})"
 
     def __str__(self):
         return " + ".join(map(str, self.equation))
+
+    def __mul__(self, other):
+        if isinstance(other, Polynomial):
+            new_poly = Polynomial()
+            new_poly.equation = self.__multiply_equations(self.equation, other.equation)
+            return new_poly
+
+    def __truediv__(self, other):
+        if isinstance(other, Polynomial):
+            pass
+
+    def __multiply_equations(self, equation_1: list, equation_2: list) -> list:
+        mult_eq = list()
+        for eq1 in equation_1:
+            for eq2 in equation_2:
+                mult_eq.append(eq1 * eq2)
+
+        # TODO - Consolidate equation
+        new_eq = list()
+        for _ in range(mult_eq[0].exponent, -1, -1):
+            coefficient = list(map(
+                lambda x: x.coefficient,
+                list(filter(lambda x: x.exponent == _, mult_eq))
+            ))
+
+            try:
+                coefficient = coefficient[0] ^ coefficient[1]
+            except IndexError:
+                coefficient = coefficient[0]
+
+            # coefficient = coefficient if coefficient < 256 else coefficient % 255
+            new_eq.append(Term(coefficient, _))
+
+        return new_eq
 
 
 class Message(Polynomial):
@@ -52,7 +119,7 @@ class Message(Polynomial):
         self.equation = self.create_message_polynomial(encoded_string)
 
     def __repr__(self):
-        return f"PloyMessage()"
+        return f"PloyMessage({self})"
 
     def create_message_polynomial(self, string: str):
         equation = []
@@ -69,9 +136,21 @@ class Generator(Polynomial):
 
     def __init__(self, code_words: int):
         super().__init__()
+        self.equation = self.create_generator_polynomial(code_words)
 
     def __repr__(self):
-        return f"PolyGenerator()"
+        return f"PolyGenerator({self})"
 
     def create_generator_polynomial(self, words: int):
-        pass
+        if words in self.generator_from_words.keys():
+            return self.generator_from_words[words]
+
+        for num in range(3, words + 1):
+            x = Polynomial()
+            y = Polynomial()
+            x.equation = self.generator_from_words[num - 1]
+            y.equation = [Term(1, 1), Term(GF.find_integer(num), 0)]
+            z = x * y
+            self.generator_from_words.update({num: z.equation})
+
+        return self.generator_from_words[words]
