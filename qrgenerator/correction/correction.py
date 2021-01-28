25a6af47f10e22da12f8e6263897cf906d5cc4f1
tim.pogue <tim.pogue@cybera.net>
2021-01-28 15:59:48 -0600

    Update to use namedtuple.

--- qrgenerator/correction/correction.py
+++ qrgenerator/correction/correction.py
@@ -1,27 +1,23 @@
-from .formatting import correction
+from .formatting.correction import *
+from .polynomial import Message, Generator
 
 
 class ErrorCorrection:
     def __init__(self, encoding: str, version: int, correction_level: str):
-        self.required_bits = self._find_required_bits_number(correction_level, version)
-        self.padded_encoding = self._pad_string(encoding)
+        self.required_bits = self.__find_required_bits_number(correction_level, version)
+        self.padded_encoding = self.__pad_string(encoding)
 
-    def _find_required_bits_number(
+    def __find_required_bits_number(
         self, correction_level: str, version_number: int
     ) -> int:
-        numbers = correction.correction_table[f"{version_number}-{correction_level}"]
+        numbers = correction_table[f"{version_number}-{correction_level}"]
 
-        group_1_block = int(numbers[1])
-        group_1_codeword = int(numbers[2])
-        group_2_block = numbers[3] if numbers[3] != "" else 0
-        group_2_codeword = numbers[4] if numbers[4] != "" else 0
+        return (numbers.blocks_group_1 + numbers.dc_group_1) + (numbers.blocks_group_2 + numbers.dc_group_2)
 
-        return (group_1_codeword * group_1_block) + (group_2_codeword * group_2_block)
-
-    def _pad_string(self, encoded_string: str) -> str:
+    def __pad_string(self, encoded_string: str) -> str:
         if len(encoded_string) == self.required_bits:
             return encoded_string
 
-        padding_bits = (self.required_bits - len(encoded_string)) / 2 + 1
-        padded_string = "".join([encoded_string, correction.filler * padding_bits])
+        padding_bits: float = (self.required_bits - len(encoded_string)) / 2 + 1
+        padded_string: str = "".join([encoded_string, filler * padding_bits])
         return padded_string[: self.required_bits]
