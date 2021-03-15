32645518ccbd4554680f8c8f9f535f1d31f1003a
Accoustium <t.pogue.python@gmail.com>
2021-03-15 13:12:10 -0500

    Update and structure final message after creating error correction words

--- qrgenerator/correction/correction.py
+++ qrgenerator/correction/correction.py
@@ -9,6 +9,22 @@ class ErrorCorrection:
         self.error_encoding: str = self.__create_error_words(correction_level, version)
         self.word_blocks: list[str] = self.__create_blocks(self.padded_encoding, correction_level, version)
         self.error_blocks: list[str] = self.__create_blocks(self.error_encoding, correction_level, version)
+        self.final_message: str = self.__structure_words(version)
+
+    def __structure_words(self, version: int):
+        final = ''
+        word_length = len(self.word_blocks)
+        error_length = len(self.error_blocks)
+        max_word = max(max(map(len, self.word_blocks)), max(map(len, self.error_blocks)))
+        for col in range(max_word):
+            for row in range(word_length):
+                final = ''.join([final, self.word_blocks[row][col]])
+            for row in range(error_length):
+                final = ''.join([final, self.error_blocks[row][col]])
+
+        final = final + '0' * remainder_bits[version]
+
+        return final
 
     def __find_required_bits_number(
         self, correction_level: str, version_number: int
@@ -34,16 +50,27 @@ class ErrorCorrection:
 
         return code_words.bin()
 
+    def __split_into_words(self, word_block: str) -> list[str]:
+        word = []
+        for length in range(0, len(word_block), 8):
+            word.append(word_block[length: length + 8])
+
+        return word
+
     def __create_blocks(self, encoding: str, correction_level: str, version_number: int) -> list[str]:
         numbers = correction_table[f"{version_number}-{correction_level}"]
         blocks = list()
 
         for block1 in range(numbers.blocks_group_1):
             start = block1 * numbers.dc_group_1 * 8
-            blocks.append(encoding[start: start + (numbers.dc_group_1 * 8)])
+            blocks.append(
+                self.__split_into_words(encoding[start: start + (numbers.dc_group_1 * 8)])
+            )
 
         for block2 in range(numbers.blocks_group_2):
             start = ((numbers.blocks_group_1 * numbers.dc_group_1) + (block2 * numbers.dc_group_2)) * 8
-            blocks.append(encoding[start: start + (numbers.dc_group_2 * 8)])
+            blocks.append(
+                self.__split_into_words(encoding[start: start + (numbers.dc_group_2 * 8)])
+            )
 
         return blocks
